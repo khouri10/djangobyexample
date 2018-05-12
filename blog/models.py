898000ba7a52c52a7391b05b7044b47e40b53d4e
Marcel <khouri10@gmail.com>
2018-05-12 20:27:35 -0300

    add url patterns

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 class PublishedManager(models.Manager):
 	def get_queryset(self):
 		return super(PublishedManager, self).get_queryset()\
 											.filter(status='published')
-											
+
 class Post(models.Model):
 	STATUS_CHOICES = (('draft', 'Draft'), ('published', 'Published'),)
 	title = models.CharField(max_length=250)
@@ -27,6 +28,13 @@ class Post(models.Model):
 
 	objects = models.Manager() # The default manager.
 	published = PublishedManager() # Our custom manager.
+
+	def get_absolute_url(self):
+		return reverse('blog:post_detail',
+						args=[self.publish.year,
+						self.publish.strftime('%m'),
+						self.publish.strftime('%d'),
+						self.slug])
 
 
 
